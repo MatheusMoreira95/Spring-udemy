ecf79a2a6726bc0f49a92614e1c0953a487a3618
Matheus Moreira <matheusjosemoreira@gmail.com>
2022-10-08 17:09:53 -0300

    Product

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -1,8 +1,11 @@
 package com.educandoweb.course.entities;
 
 import jakarta.persistence.*;
+import jdk.jfr.TransitionTo;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -13,6 +16,10 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+
+    @Transient
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
     }
@@ -38,6 +45,10 @@ public class Category implements Serializable {
         this.name = name;
     }
 
+
+    public Set<Product> getProducts() {
+        return products;
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -50,6 +61,7 @@ public class Category implements Serializable {
     public int hashCode() {
         return id.hashCode();
     }
+
 
 
 }
