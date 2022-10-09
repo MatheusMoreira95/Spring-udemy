9bbfe0cd4c7f5a1ada8558cc3d6360b6c37f3001
Matheus Moreira <matheusjosemoreira@gmail.com>
2022-10-09 00:10:44 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -6,6 +6,8 @@ import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Entity
@@ -25,6 +27,9 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
+
+    @OneToMany(mappedBy = "id.order" )
+    private Set<OrderItem> items = new HashSet<>();
     public Order() {
     }
 
@@ -69,6 +74,9 @@ public class Order implements Serializable {
         this.client = client;
     }
 
+    public Set<OrderItem> getItems(){
+        return items;
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
---
+++ src/main/java/com/educandoweb/course/entities/pk/OrderItemPK.java
@@ -0,0 +1,54 @@
+package com.educandoweb.course.entities.pk;
+
+import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serializable;
+
+
+@Embeddable
+public class OrderItemPK implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof OrderItemPK that)) return false;
+
+        if (!order.equals(that.order)) return false;
+        return product.equals(that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = order.hashCode();
+        result = 31 * result + product.hashCode();
+        return result;
+    }
+}
