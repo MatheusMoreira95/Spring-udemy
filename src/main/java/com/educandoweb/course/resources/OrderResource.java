584d566a410af1cbca75e0ddd95a329455e40985
Matheus Moreira <matheusjosemoreira@gmail.com>
2022-10-10 16:55:42 -0300

    exceções e order insert

--- src/main/java/com/educandoweb/course/resources/OrderResource.java
+++ src/main/java/com/educandoweb/course/resources/OrderResource.java
@@ -1,14 +1,16 @@
 package com.educandoweb.course.resources;
 
 import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.Product;
+import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.OrderService;
+import com.educandoweb.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -17,17 +19,26 @@ public class OrderResource {
 
     @Autowired
     private OrderService service;
+    private UserService user;
 
     @GetMapping
-    public ResponseEntity<List<Order>> findAll(){
+    public ResponseEntity<List<Order>> findAll() {
         List<Order> list = service.findAll();
-       return ResponseEntity.ok().body(list);
+        return ResponseEntity.ok().body(list);
     }
 
     @GetMapping(value = "/{Id}")
-    public ResponseEntity<Order> findById(@PathVariable Long Id){
+    public ResponseEntity<Order> findById(@PathVariable Long Id) {
         Order obj = service.findById(Id);
 
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Order> insert(@RequestBody Order obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("{id}")
+                .buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
--- src/main/java/com/educandoweb/course/services/OrderService.java
+++ src/main/java/com/educandoweb/course/services/OrderService.java
@@ -1,8 +1,13 @@
 package com.educandoweb.course.services;
 
 import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.OrderRepository;
+import com.educandoweb.course.services.exceptions.DatabaseException;
+import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,13 +19,20 @@ public class OrderService {
     @Autowired
     private OrderRepository repository;
 
-    public List<Order> findAll(){
+    public List<Order> findAll() {
         return repository.findAll();
     }
-    public Order findById(Long Id){
-       Optional<Order> obj= repository.findById(Id);
-       return obj.get();
+
+    public Order findById(Long Id) {
+        Optional<Order> obj = repository.findById(Id);
+        return obj.get();
     }
 
-
+    public Order insert(Order obj) {
+        try {
+            return repository.save(obj);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
 }
